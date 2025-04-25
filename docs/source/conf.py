305aafa3c4cdaa521e53e47742f20cf9f43a527a
Françoise Conil <fcodvpt@gmail.com>
2025-04-25 18:56:02 +0200

    Refactor Sphinx documentation organisation
    Use a source folder for the reStructuredText files, included the
    generated files.
    
    - What's the purpose of the fractal_tree.graffle file ?
    - Tutorial.zip file should probable be removed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,11 +6,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'structify_net'
-copyright = '2023, Rémy Cazabet'
+project = 'structify-net'
+copyright = '2025, Rémy Cazabet'
 author = 'Rémy Cazabet'
 release = '2'
 
+"""
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
@@ -26,15 +27,16 @@ class Mock(MagicMock):
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate',"seaborn","pandas","scipy.special","scipy.spatial","tqdm","tqdm.auto","networkx","math","numbers","random"]
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
+"""
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc','sphinx.ext.autosummary',"sphinx_rtd_theme"]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.napoleon', 'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
+
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -46,4 +48,4 @@ html_css_files = [
     'custom.css',
 ]
 
-autosummary_generate = True
+# autosummary_generate = True
